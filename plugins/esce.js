14bcb94c35c5b1bb264119b590d978cf8cf47a75
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-09-05 04:46:20 +0700

    Update esce.js

--- plugins/esce.js
+++ plugins/esce.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, command }) => {
 • Maka Saran gunakan replit atau jenis run yang bisa save file terlebih dahulu lalu download file sessions nya.
 • Biasanya memiliki 50 sesi atau bisa lebih
 • Upload sesi tersebut ke github lalu run ulang.
-• *Jika ingin melihat apa itu multipel session lihat link di bawah ini.*
+• *Jika ingin melihat apa itu multiple session lihat link di bawah ini.*
 • *Preview* : https://telegra.ph/file/fd3362fea5132cc59fed2.jpg 
 • *Preview* : https://telegra.ph/file/fa2a66e8cca50f62885ab.jpg
 `, m)
