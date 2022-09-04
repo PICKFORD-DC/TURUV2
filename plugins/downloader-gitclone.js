caf4a3b0db3c840472b6073fafacdb0f0b4eeca4
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-09-05 05:31:27 +0700

    Update downloader-gitclone.js

--- plugins/downloader-gitclone.js
+++ plugins/downloader-gitclone.js
@@ -1,20 +1,28 @@
 import fetch from 'node-fetch'
-const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
-let handler = async (m, { args, usedPrefix, command }) => {
-    if (!args[0]) throw `Example user ${usedPrefix}${command} https://github.com/BochilGaming/games-wabot`
-    if (!regex.test(args[0])) throw 'link salah!'
-    let [_, user, repo] = args[0].match(regex) || []
-    repo = repo.replace(/.git$/, '')
-    let url = `https://api.github.com/repos/${user}/${repo}/zipball`
-    let filename = (await fetch(url, { method: 'HEAD' })).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1]
-    // 'attachment; filename=Nurutomo-wabot-aq-v2.5.1-251-g836cccd.zip'
-    m.reply(`*Mohon tunggu, sedang mengirim repository..*`)
-    conn.sendFile(m.chat, url, filename, null, m)
-}
-handler.help = ['gitclone <url>']
-handler.tags = ['downloader']
-handler.command = /gitclone/i
 
+const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+	if (!args[0]) throw `Example user ${usedPrefix}${command} https://github.com/BOTCAHX/RTXZ-MD`
+	if (!regex.test(args[0])) throw 'Invalid repositories!'
+	try {
+		let [_, user, repo] = args[0].match(regex) || []
+		repo = repo.replace(/.git$/, '')
+		let url = `https://api.github.com/repos/${user}/${repo}/zipball`
+		let filename = (await fetch(url, { method: 'HEAD' })).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1]
+		m.reply(`*_sending file, don't spam . . ._*`)
+		conn.sendFile(m.chat, url, filename.replace('.zip.zip','.zip'), null, m)
+	} catch (e) {
+		console.log(e)
+		m.reply(`[!] repositorie(s) not found.`)
+	}
+}
+
+handler.menudownload = ['gitclone']
+handler.tagsdownload = ['search']
+handler.command = /^(gitclone)$/i
+
+handler.premium = true
 handler.limit = true
 
 export default handler
