80ef2d4b9fdbb704063c20260bc89e9128b880b0
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-09-10 03:11:00 +0700

    Update yt-play.js

--- plugins/yt-play.js
+++ plugins/yt-play.js
@@ -1,22 +1,27 @@
+// ID
 // by BOTCAHX
-// entahlah gua masih belajar import gini
+// entahlah gua masih belajar base ESM gini
 // klo error lapor aja kecuali apikey abis ya jangan laporan
+// EN
+// by BOTCAHX
+// I don't know I'm still learning base ESM like this
+// if there is an error, just report it unless the apikey is finished, don't report it
 
 import fetch from 'node-fetch'
 let handler = async (m, {command, conn, text}) => {
-if (!text) throw `Gunakan :\n*.play dj tiktok*\n*.playvid dj tiktok*\n\n*[ .play ]* Untuk download audio\n*[ .playvid ]* Untuk download video`
+if (!text) throw `Use :\n*.play dj Fungky*\n*.playvid dj Fungky*\n\n*[ .play ]* To download audios\n*[ .playvid ]* To download videos`
 try {
 let res = await fetch(`https://api.lolhuman.xyz/api/ytplay2?apikey=85faf717d0545d14074659ad&query=${text}`)
 if (command == 'play') {
-conn.reply(m.chat, `*Proses Sedang berjalan...*`, m)  
+conn.reply(m.chat, `*Process in progress...*`, m)  
 let json = await res.json()
 conn.sendFile(m.chat, json.result.audio, 'apikey:v.mp3', null, m, false, { mimetype: 'audio/mp4' })}
 if (command == 'playvid') {
-conn.reply(m.chat, `*Sedang Di proses...*`, m)
+conn.reply(m.chat, `*Being processed...*`, m)
 let json = await res.json()
 conn.sendFile(m.chat, json.result.video, 'error.mp4', `Nih kak :)`, m)}
 } catch (e) {
-m.reply('*Server Eror*')
+m.reply('*Server Error*')
 }}
 handler.help = ['play', 'playvid'].map(v => v + ' <judul>')
 handler.tags = ['downloader']
