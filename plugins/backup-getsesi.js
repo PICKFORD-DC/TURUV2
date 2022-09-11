4d6e298202bbd6a01fea5034289c15a2b197a505
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-09-12 06:58:43 +0700

    Update backup-getsesi.js

--- plugins/backup-getsesi.js
+++ plugins/backup-getsesi.js
@@ -12,22 +12,21 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 		let hours = date_ob.getHours();
 		let minutes = date_ob.getMinutes();
 		let seconds = date_ob.getSeconds();
-		let ini_txt = `${year + month + date + "_" + hours + minutes + seconds}`
 
 		let database = await fs.readFileSync(`./database.json`)
 		let session = await fs.readFileSync(`./data.store.json`)
 		let sessions = await fs.readFileSync(`./media/backup.zip`)
-		await conn.sendMessage(m.chat, {document: database, mimetype: 'application/json', fileName: `database${ini_txt}.json`}, { quoted : m })
-		await conn.sendMessage(m.chat, {document: session, mimetype: 'application/json', fileName: `data.store${ini_txt}.json`}, { quoted : m })
-		await conn.sendMessage(m.chat, {document: sessions, mimetype: 'application/zip', fileName: `sessions${ini_txt}.zip`}, { quoted : m })
+		await conn.sendMessage(m.chat, {document: database, mimetype: 'application/json', fileName: `database.json`}, { quoted : m })
+		await conn.sendMessage(m.chat, {document: session, mimetype: 'application/json', fileName: `data.store.json`}, { quoted : m })
+		await conn.sendMessage(m.chat, {document: sessions, mimetype: 'application/zip', fileName: `sessions.zip`}, { quoted : m })
 	} catch (e) {
 		console.log(e)
 		m.reply(`Terjadi kesalahan, coba lagi.`)
 	}
 }
 
-handler.help = ['database']
-handler.tags = ['search']
+handler.help = ['backup', 'getsesi', 'database']
+handler.tags = ['owner']
 handler.command = /^(backup|getsesi|database)$/i
 
 handler.owner = true
