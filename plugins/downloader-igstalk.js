34c891a91841b69e70e262c633cba4c375a71ecd
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-09-11 12:17:45 +0700

    Update downloader-igstalk.js

--- plugins/downloader-igstalk.js
+++ plugins/downloader-igstalk.js
@@ -1,28 +1,55 @@
-import { instagramStalk } from '@bochilteam/scraper'
+import cheerio from 'cheerio'
+import fetch from 'node-fetch'
 
-let handler= async (m, { args, usedPrefix, command }) => {
-    if (!args[0]) throw `Example use ${usedPrefix}${command} <username>`
-    const {
-        username,
-        name,
-        description,
-        followersH,
-        followingH,
-        postsH,
-    } = await instagramStalk(args[0])
-    m.reply(`
-${name} *(${username})*
-https://instagram.com/${username.replace(/^@/, '')}
-*${followersH}* Followers
-*${followingH}* Following
-*${postsH}* Posts
-*Bio:* ${description}
-`.trim())
+let handler = async (m, { conn, text }) => {
+  if (!text) throw 'Input username'
+  let res = await igStalk(text)
+  if (!res) throw res
+  let caption = `
+*Name:* ${res.name}
+*Username:* ${res.username}
+*Followers:* ${res.followersH}
+*Following:* ${res.followingH}
+*Posts:* ${res.postsH}
+*Bio:*
+${res.description}
+`.trim()
+  if (res.profilePic) return conn.sendMessage(m.chat, { image: { url: res.profilePic }, caption }, { quoted: m })
+  m.reply(caption)
 }
-
-handler.help = ['igstalk'].map(v => v + ' <username>')
-handler.tags = ['downloader']
-
-handler.command = /^(igstalk)$/i
+handler.help = ['igstalk']
+handler.tags = ['tools']
+handler.alias = ['igstalk', 'stalkig']
+handler.command = /^(igstalk|stalkig)$/i
 
 export default handler
+
+async function igStalk(username) {
+    username = username.replace(/^@/, '')
+    const html = await (await fetch(`https://dumpor.com/v/${username}`)).text()
+    const $$ = cheerio.load(html)
+    const name = $$('div.user__title > a > h1').text().trim()
+    const Uname = $$('div.user__title > h4').text().trim()
+    const description = $$('div.user__info-desc').text().trim()
+    const profilePic = $$('div.user__img').attr('style')?.replace("background-image: url('", '').replace("');", '')
+    const row = $$('#user-page > div.container > div > div > div:nth-child(1) > div > a')
+    const postsH = row.eq(0).text().replace(/Posts/i, '').trim()
+    const followersH = row.eq(2).text().replace(/Followers/i, '').trim()
+    const followingH = row.eq(3).text().replace(/Following/i, '').trim()
+    const list = $$('ul.list > li.list__item')
+    const posts = parseInt(list.eq(0).text().replace(/Posts/i, '').trim().replace(/\s/g, ''))
+    const followers = parseInt(list.eq(1).text().replace(/Followers/i, '').trim().replace(/\s/g, ''))
+    const following = parseInt(list.eq(2).text().replace(/Following/i, '').trim().replace(/\s/g, ''))
+    return {
+        name,
+        username: Uname,
+        description,
+        postsH,
+        posts,
+        followersH,
+        followers,
+        followingH,
+        following,
+        profilePic
+    }
+}
