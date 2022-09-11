ac2f8644b31c6416269230e0c85fbdeddd42260b
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-09-12 06:57:30 +0700

    Update esce.js

--- plugins/esce.js
+++ plugins/esce.js
@@ -5,9 +5,9 @@ let handler = async (m, { conn, command }) => {
     conn.reply(m.chat, `*BETA V2.0.1.6*
 • *Multi Auth ( Multiple File Session )*
 • https://github.com/BOTCAHX/RT-X-MD
-• Script ini mempunyai sesi yang banyak
-• Maka Saran gunakan replit atau jenis run yang bisa save file terlebih dahulu lalu download file sessions nya.
-• Upload sesi tersebut ke github lalu run ulang.
+• Script ini mempunyai miltiple session 
+• Maka bila ingin mengambil session nya cukup dengan scan dan ketik di kolom chat bot .getsesi
+• Upload sesi yang di kirim bot mu ke github lalu run ulang.
 • *Jika ingin melihat apa itu multiple session lihat link di bawah ini.*
 • *Preview* : https://telegra.ph/file/fd3362fea5132cc59fed2.jpg 
 • *Preview* : https://telegra.ph/file/fa2a66e8cca50f62885ab.jpg
