b408b53df9eb3e4610aacc233a7d17a109fd082e
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-09-13 02:08:29 +0700

    Update join.js

--- plugins/join.js
+++ plugins/join.js
@@ -1,22 +1,23 @@
-import db from '../lib/database.js'
+let linkRegex = /([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
 
-let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
+let handler = async (m, { conn, text, usedPrefix, command, isOwner }) => {
 
-let handler = async (m, { conn, text, isOwner }) => {
     let [_, code, expired] = text.match(linkRegex) || []
     if (!code) throw 'Link invalid'
     let res = await conn.groupAcceptInvite(code)
-    expired = Math.floor(Math.min(999, Math.max(1, isOwner ? isNumber(expired) ? parseInt(expired) : 0 : 3)))
-    m.reply(`Berhasil join grup ${res}${expired ? ` selama ${expired} hari` : ''}`)
-    let chats = db.data.chats[res]
-    if (!chats) chats = db.data.chats[res] = {}
+    if (!res) throw res.toString()
+    let name = await conn.getName(res).catch(_ => null)
+    
+    let caption = `*Berhasil join grup* ${name || res}\n`
+  
+    let chats = global.db.data.chats[res]
+    if (!chats) chats = global.db.data.chats[res] = {}
     if (expired) chats.expired = +new Date() + expired * 1000 * 60 * 60 * 24
+
+return conn.sendButton(m.chat,caption, author, 'Ok', 'y sj') 
 }
-handler.help = ['join <chat.whatsapp.com>']
-handler.tags = ['premium']
-
+handler.help = ['join']
+handler.tags = ['group']
 handler.command = /^join$/i
-
+handler.premium = true
 export default handler
-
-const isNumber = (x) => (x = parseInt(x), typeof x === 'number' && !isNaN(x))
