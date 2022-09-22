fa8299f90b7d98a8cbd4b363c52edbca613a4c9c
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-09-23 06:23:06 +0700

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -3,7 +3,7 @@
 //const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
 //export async function before(m, { conn, isAdmin, isBotAdmin }) {
     //if (m.isBaileys && m.fromMe)
-        return !0
+        //return !0
     //if (!m.isGroup) return !1
     //let chat = db.data.chats[m.chat]
     //let bot = db.data.settings[this.user.jid] || {}
