bff9cfcb5f024d2e89429c03b6efeb5bb9edcfa0
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-09-24 02:45:14 +0700

    Update _anti+212.js

--- plugins/_anti+212.js
+++ plugins/_anti+212.js
@@ -1,14 +1,16 @@
+//ubah ke true bila mau 
+
 let handler = m => m
 
 handler.before = async function (m) {
    if (m.sender.startsWith('212' || '212')) {
-   	global.db.data.users[m.sender].banned = true
+   	global.db.data.users[m.sender].banned = false 
 conn.reply('see u next time')
 conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
    }
    
    if (m.sender.startsWith('265' || '265')) {
-   	global.db.data.users[m.sender].banned = true
+   	global.db.data.users[m.sender].banned = false
 conn.reply('see u next time')
 conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
    } 
